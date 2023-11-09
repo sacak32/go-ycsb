d1e94285c8f52db433a48e4818c3a660b90ad2a7
sacak32 <sacak32@node-3.sacak32-177326.iacoma23-pg0.apt.emulab.net>
2023-11-09 10:44:47 -0700

    setup added

---
+++ setup.sh
@@ -0,0 +1,6 @@
+curl -OL https://golang.org/dl/go1.21.4.linux-amd64.tar.gz
+sha256sum go1.21.4.linux-amd64.tar.gz
+sudo tar -C /usr/local -xvf go1.21.4.linux-amd64.tar.gz
+rm go1.21.4.linux-amd64.tar.gz
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
+source ../.profile
