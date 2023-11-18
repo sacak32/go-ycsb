4d29ec2b2794bfb9a24ef38d3ccc50fd70c5d97c
sacak32 <sacak32@node0.clients.uiuc-ss-pg0.utah.cloudlab.us>
2023-11-17 19:04:57 -0700

    addition

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -OL https://golang.org/dl/go1.21.4.linux-amd64.tar.gz
-sha256sum go1.21.4.linux-amd64.tar.gz
-sudo tar -C /usr/local -xvf go1.21.4.linux-amd64.tar.gz
-rm go1.21.4.linux-amd64.tar.gz
+curl -OL https://golang.org/dl/go1.21.4.linux-arm64.tar.gz
+sha256sum go1.21.4.linux-arm64.tar.gz
+sudo tar -C /usr/local -xvf go1.21.4.linux-arm64.tar.gz
+rm go1.21.4.linux-arm64.tar.gz
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
 source ../.profile
